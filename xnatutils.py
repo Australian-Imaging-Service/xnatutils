c2e701d38dae219630b5d15df6868bd559d7c2bc
Tom Close <tclose@oist.jp>
2017-02-14 17:06:45 +1100

    implemented xnat-download

--- xnatutils.py
+++ xnatutils.py
@@ -3,21 +3,49 @@ import xnat
 
 MBI_XNAT_SERVER = 'https://mbi-xnat.erc.monash.edu.au'
 
-data_formats_by_ext = {
-    'nii': 'NIFTI',
-    'nii.gz': 'NIFTI_GZ',
-    'mif': 'MRTRIX'}
+data_format_exts = {
+    'NIFTI': '.nii',
+    'NIFTI_GZ': '.nii.gz',
+    'MRTRIX': '.mif',
+    'DICOM': ''}
 
 
-def connect():
+class XnatUtilsUsageException(Exception):
+    pass
+
+
+def connect(user=None):
+    if user is not None:
+        raise NotImplementedError(
+            "Specifying a different username is not currently supported. "
+            " Please store authcate and password in ~/.netrc (with permissions"
+            " 600) in the format (see https://xnat.readthedocs.io/en/latest/"
+            "static/tutorial.html#credentials):\n"
+            "\n"
+            "machine mbi-xnat.erc.monash.edu.au\n"
+            "user <your-authcate>\n"
+            "password <your-authcate-password>\n")
     return xnat.connect(MBI_XNAT_SERVER)
 
 
-def get_data_format(filename):
+def extract_extension(filename):
     name_parts = os.path.basename(filename).split('.')
-    if name_parts[-1] == 'gz':
-        num_parts = 2
+    if len(name_parts) == 1:
+        ext = ''
     else:
-        num_parts = 1
-    ext = '.'.join(name_parts[-num_parts:])
-    return data_formats_by_ext[ext]
+        if name_parts[-1] == 'gz':
+            num_parts = 2
+        else:
+            num_parts = 1
+        ext = '.' + '.'.join(name_parts[-num_parts:])
+    return ext
+
+
+def get_data_format(filename):
+    try:
+        return next(k for k, v in data_format_exts.iteritems()
+                    if v == extract_extension(filename))
+    except StopIteration:
+        raise XnatUtilsUsageException(
+            "No format matching extension '{}' (of '{}')"
+            .format(extract_extension(filename), filename))
