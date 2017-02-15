edb59cdf0f08c3ee7b6d5f445c9f27713fc853e0
Tom Close <tclose@oist.jp>
2017-02-15 16:55:47 +1100

    added askpass to connect function and implemented fast listing of projects/subjects/sessions/scans

--- xnatutils.py
+++ xnatutils.py
@@ -1,4 +1,7 @@
 import os.path
+import re
+import stat
+import getpass
 import xnat
 
 MBI_XNAT_SERVER = 'https://mbi-xnat.erc.monash.edu.au'
@@ -10,22 +13,29 @@ data_format_exts = {
     'DICOM': ''}
 
 
-class XnatUtilsUsageException(Exception):
+class XnatUtilsUsageError(Exception):
     pass
 
 
 def connect(user=None):
-    if user is not None:
-        raise NotImplementedError(
-            "Specifying a different username is not currently supported. "
-            " Please store authcate and password in ~/.netrc (with permissions"
-            " 600) in the format (see https://xnat.readthedocs.io/en/latest/"
-            "static/tutorial.html#credentials):\n"
-            "\n"
-            "machine mbi-xnat.erc.monash.edu.au\n"
-            "user <your-authcate>\n"
-            "password <your-authcate-password>\n")
-    return xnat.connect(MBI_XNAT_SERVER)
+    netrc_path = os.path.join(os.environ['HOME'], '.netrc')
+    if user is not None or not os.path.exists(netrc_path):
+        if user is None:
+            user = raw_input('username: ')
+        password = getpass.getpass()
+        save_netrc = raw_input(
+            "Would you like to save this username/password in your ~/.netrc "
+            "(with 600 permissions) [Y/n]: ")
+        if save_netrc.lower() not in ('n', 'no'):
+            with open(netrc_path, 'w') as f:
+                f.write(
+                    "machine {}\n".format(MBI_XNAT_SERVER.split('/')[-1]) +
+                    "user {}\n".format(user) +
+                    "password {}\n".format(password))
+            os.chmod(netrc_path, stat.S_IRUSR | stat.S_IWUSR)
+    kwargs = ({'user': user, 'password': password}
+              if not os.path.exists(netrc_path) else {})
+    return xnat.connect(MBI_XNAT_SERVER, **kwargs)
 
 
 def extract_extension(filename):
@@ -46,10 +56,32 @@ def get_data_format(filename):
         return next(k for k, v in data_format_exts.iteritems()
                     if v == extract_extension(filename))
     except StopIteration:
-        raise XnatUtilsUsageException(
+        raise XnatUtilsUsageError(
             "No format matching extension '{}' (of '{}')"
             .format(extract_extension(filename), filename))
 
 
 def get_extension(data_format):
     return data_format_exts[data_format]
+
+
+def is_regex(string):
+    "Checks to see if string contains special characters"
+    return not bool(re.match(r'\w+', string))
+
+
+def list_results(mbi_xnat, path):
+    return mbi_xnat.get('/data/archive/' + path).json()['ResultSet']['Result']
+
+
+def matching_sessions(mbi_xnat, session_ids):
+    if isinstance(session_ids, basestring):
+        session_ids = [session_ids]
+    all_sessions = [s['label'] for s in list_results(mbi_xnat, 'experiments')]
+    return [s for s in all_sessions
+            if any(re.match(i, s) for i in session_ids)]
+
+
+if __name__ == '__main__':
+    with connect() as mbi_xnat:
+        print '\n'.join(matching_sessions(mbi_xnat, 'MRH06.*_MR01'))
