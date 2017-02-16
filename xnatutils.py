feca9b02bbdfc20ef8369076aa871c06b9f82e95
Tom Close <tclose@oist.jp>
2017-02-16 16:25:20 +1100

    completed xnat-ls

--- xnatutils.py
+++ xnatutils.py
@@ -25,14 +25,16 @@ def connect(user=None):
         password = getpass.getpass()
         save_netrc = raw_input(
             "Would you like to save this username/password in your ~/.netrc "
-            "(with 600 permissions) [Y/n]: ")
-        if save_netrc.lower() not in ('n', 'no'):
+            "(with 600 permissions) [y/N]: ")
+        if save_netrc.lower() in ('y', 'yes'):
             with open(netrc_path, 'w') as f:
                 f.write(
                     "machine {}\n".format(MBI_XNAT_SERVER.split('/')[-1]) +
                     "user {}\n".format(user) +
                     "password {}\n".format(password))
             os.chmod(netrc_path, stat.S_IRUSR | stat.S_IWUSR)
+            print ("MBI-XNAT username and password for user '{}' saved in {}"
+                   .format(user, os.path.join(os.environ['HOME'], '.netrc')))
     kwargs = ({'user': user, 'password': password}
               if not os.path.exists(netrc_path) else {})
     return xnat.connect(MBI_XNAT_SERVER, **kwargs)
@@ -69,7 +71,7 @@ def is_regex(ids):
     "Checks to see if string contains special characters"
     if isinstance(ids, basestring):
         ids = [ids]
-    return not all(re.match(r'\w+', i) for i in ids)
+    return not all(re.match(r'^\w+$', i) for i in ids)
 
 
 def list_results(mbi_xnat, path, attr):
@@ -82,15 +84,49 @@ def list_results(mbi_xnat, path, attr):
     return results
 
 
+def matching_subjects(mbi_xnat, subject_ids):
+    if is_regex(subject_ids):
+        all_subjects = list_results(mbi_xnat, 'subjects', attr='label')
+        subjects = [s for s in all_subjects
+                    if any(re.match(i, s) for i in subject_ids)]
+    else:
+        subjects = set()
+        for id_ in subject_ids:
+            subjects.update(
+                list_results(
+                    mbi_xnat, 'projects/{}/subjects'.format(id_), 'label'))
+        subjects = list(subjects)
+    return subjects
+
+
 def matching_sessions(mbi_xnat, session_ids):
     if isinstance(session_ids, basestring):
         session_ids = [session_ids]
     if is_regex(session_ids):
         all_sessions = list_results(mbi_xnat, 'experiments', attr='label')
-        return [s for s in all_sessions
-                if any(re.match(i, s) for i in session_ids)]
+        sessions = [s for s in all_sessions
+                    if any(re.match(i, s) for i in session_ids)]
     else:
-        return session_ids
+        sessions = set()
+        for id_ in session_ids:
+            if '_' not in id_:
+                project = mbi_xnat.projects[id_]
+                sessions.update(list_results(
+                    mbi_xnat, 'projects/{}/experiments'.format(project.id),
+                    'label'))
+            elif id_ .count('_') == 1:
+                subject = mbi_xnat.subjects[id_]
+                sessions.update(list_results(
+                    mbi_xnat, 'subjects/{}/experiments'.format(subject.id),
+                    'label'))
+            elif id_ .count('_') == 2:
+                sessions = sessions.add(id_)
+            else:
+                raise XnatUtilsUsageError(
+                    "Invalid ID '{}' for listing sessions "
+                    .format(id_))
+        sessions = list(sessions)
+    return sessions
 
 
 if __name__ == '__main__':
