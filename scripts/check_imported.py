c18ae713100ee4c0984cde74d692dc42811c57f2
Tom Close <tom.g.close@gmail.com>
2017-04-26 14:26:33 +1000

    implemented check_imported script

--- scripts/check_imported.py
+++ scripts/check_imported.py
@@ -5,54 +5,85 @@ import os.path
 import shutil
 import tempfile
 import getpass
+import logging
+import hashlib
+import sys
 from nianalysis.archive.daris import DarisLogin
+
+resources_path = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), '_resources'))
+sys.path.insert(0, resources_path)
+from mbi_to_daris_number import mbi_to_daris  # @IgnorePep8 @UnresolvedImport
+
+
+url_prefix = 'file:/srv/mediaflux/mflux/volatile/stores/pssd/'
+daris_store_prefix = '/mnt/rdsi/mf-data/stores/pssd'
+xnat_store_prefix = '/mnt/vicnode/archive/'
+
+
+def checksum(fpath):
+    return hashlib.md5(open(fpath, 'rb').read()).hexdigest()
 
 parser = ArgumentParser()
 parser.add_argument('project', type=str,
                     help='ID of the project to import')
-parser.add_argument('--subjects', nargs='+', default=None, type=int,
-                    help="IDs of the subjects to import")
+parser.add_argument('--log_file', type=str, default=None,
+                    help='Path of the logfile to record discrepencies')
+parser.add_argument('--display_log', action='store_true',
+                    help="Whether to print log to screen")
 args = parser.parse_args()
 
-
-fm2darisID = {
-    'MRH055': 100, 'MRH061': 101, 'MRH062': 105, 'MRH063': 106, 'MRH064': 107,
-    'MRH065': 110, 'MRH066': 112, 'MRH067': 115, 'MRH068': 116, 'MRH069': 117,
-    'MRH070': 118, 'MRH071': 119, 'MRH011': 12, 'MRH023': 12, 'MRH072': 120,
-    'MRH073': 123, 'MRH074': 124, 'MRH075': 125, 'MRH076': 126, 'MRH077': 127,
-    'MRH078': 128, 'MRH079': 129, 'MRH080': 134, 'MRH081': 137, 'MMH002': 138,
-    'MRH082': 139, 'MRH012': 14, 'MRH083': 143, 'MRH084': 145, 'MMH003': 147,
-    'MRH001': 17, 'MRH018': 17, 'MRH019': 21, 'MRH015': 22, 'MRH003': 23,
-    'MRH006': 25, 'MRH007': 27, 'MRH028': 28, 'MRH000': 3, 'MRH024': 30,
-    'MRH009': 32, 'MRH021': 33, 'MRH026': 34, 'MRH022': 36, 'MRH025': 38,
-    'MRH027': 44, 'MRH031': 45, 'MRH034': 48, 'MRH033': 49, 'MRH030': 52,
-    'MRH036': 53, 'MRH032': 54, 'MRH037': 56, 'MRH035': 57, 'MRH032': 58,
-    'MRH038': 60, 'MRH039': 62, 'MRH040': 63, 'MRH041': 65, 'MRH042': 66,
-    'MRH043': 68, 'MRH044': 69, 'MRH045': 70, 'MRH017': 71, 'MRH046': 72,
-    'MRH047': 73, 'MRH048': 74, 'MRH049': 77, 'MRH051': 81, 'MRH054': 92,
-    'MRH056': 95, 'MRH057': 96, 'MRH058': 97, 'MRH059': 98, 'MRH060': 99,
-    'MMH000': 133, 'MMH001': 144, 'MRA027': 130, 'MMA003': 146}
-
-url_prefix = 'file:/srv/mediaflux/mflux/volatile/stores/pssd/'
-store_prefix = '/mnt/rdsi/mf-data/stores/pssd'
+if args.project.startswith('MRH'):
+    modality = 'MR'
+elif args.project.startswith('MMH'):
+    modality = 'MRPT'
+else:
+    assert False, "Unrecognised modality {}".format(args.project)
 
 tmp_dir = tempfile.mkdtemp()
 
 password = getpass.getpass("DaRIS manager password: ")
 
+log_path = args.log_file if args.log_file else os.path.join(
+    os.getcwd(), '{}_checksum.log'.format(args.project))
+logger = logging.getLogger('check_imported')
+
+
+file_handler = logging.FileHandler(log_path)
+file_handler.setLevel(logging.ERROR)
+file_handler.setFormatter(logging.Formatter("%(message)s"))
+logger.addHandler(file_handler)
+
+
+if args.display_log:
+    stdout_handler = logging.StreamHandler()
+    stdout_handler.setLevel(logging.INFO)
+    stdout_handler.setFormatter(logging.Formatter(
+        "%(levelname)s - %(message)s"))
+    logger.addHandler(stdout_handler)
+
 with DarisLogin(domain='system', user='manager',
-                password=password) as daris:
-    project_daris_id = fm2darisID[args.project]
+                password=password) as daris, open(log_path, 'w') as log_file:
+    project_daris_id = mbi_to_daris[args.project]
     datasets = daris.query(
         "cid starts with '1008.2.{}' and model='om.pssd.dataset'"
         .format(project_daris_id), cid_index=True)
     cids = sorted(datasets.iterkeys(),
                   key=lambda x: tuple(int(p) for p in x.split('.')))
     for cid in cids:
-        subject_id, method_id, study_id = (int(i) for i in cid.split('.')[3:6])
-        if method_id == 1 and (args.subjects is None or
-                               subject_id in args.subjects):
-            src_zip_path = os.path.join(store_prefix,
+        subject_id, method_id, study_id, dataset_id = (
+            int(i) for i in cid.split('.')[3:])
+        if method_id == 1:
+            xnat_session = '{}_{:03}_{}{:02}'.format(
+                args.project, subject_id, modality, study_id)
+            xnat_path = os.path.join(
+                xnat_store_prefix, args.project, 'arc001', xnat_session,
+                'SCANS', str(dataset_id), 'DICOM')
+            if not os.path.exists(xnat_path):
+                logger.error('{}: missing dataset ({} - {})\n'.format(
+                    cid, xnat_session, xnat_path))
+                continue
+            src_zip_path = os.path.join(daris_store_prefix,
                                         datasets[cid].url[len(url_prefix):])
             unzip_path = os.path.join(tmp_dir, cid)
             shutil.rmtree(unzip_path, ignore_errors=True)
@@ -60,18 +91,21 @@ with DarisLogin(domain='system', user='manager',
             # Unzip DICOMs
             sp.check_call('unzip {} -d {}'.format(src_zip_path, unzip_path),
                           shell=True)
-            # Modify DICOMs to insert object identification information
-            sp.check_call(
-                'dcmodify -i "(0010,4000)=project: {project}; subject: '
-                '{project}_{subject}; session: {project}_{subject}_{session}" '
-                '{path}/*.dcm'.format(
-                    project=args.project, subject=str(subject_id).zfill(3),
-                    session=(
-                        ('MRPT' if args.project.startswith('MMH') else 'MR') +
-                        str(study_id).zfill(2)), path=unzip_path), shell=True)
-            # Send dicoms to XNAT's dicom C-Store receiver on localhost
-            sp.check_call(
-                'dcmsend -aet DARISIMPORT -aec XNAT localhost 8104 '
-                '{}/*.dcm'.format(unzip_path), shell=True)
+            match = True
+            for fname in os.listdir(unzip_path):
+                if fname.endswith('.dcm'):
+                    daris_md5 = checksum(os.path.join(unzip_path, fname))
+                    try:
+                        xnat_md5 = checksum(os.path.join(xnat_path, fname))
+                    except OSError:
+                        logger.error('{}: missing file ({}.{})\n'.format(
+                            cid, xnat_session, fname))
+                        match = False
+                    if daris_md5 != xnat_md5:
+                        logger.error('{}: incorrect checksum ({}.{})\n'
+                                     .format(cid, xnat_session, fname))
+                        match = False
+            if match:
+                logger.info('{}: matches ({})'.format(cid, xnat_session))
             shutil.rmtree(unzip_path, ignore_errors=True)
     shutil.rmtree(tmp_dir)
