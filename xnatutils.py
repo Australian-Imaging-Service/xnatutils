1345082b94af8c32637803ae9465d8746f95d61b
Tom Close <tom.g.close@gmail.com>
2017-06-07 11:27:23 +1000

    Added xnat-rename util

--- xnatutils.py
+++ xnatutils.py
@@ -495,6 +495,38 @@ def put(filename, session, scan, overwrite=False, create_session=False,
             filename, session, scan)
 
 
+def rename(session_name, new_session_name, user=None, connection=None,
+           loglevel='ERROR'):
+    """
+    Renames a session from the command line (if there has been a mistake in its
+    name for example).
+
+        >>> xnatutils.rename('MMA003_001_MR01', 'MMA003_001_MRPT01')
+
+    Parameters
+    ----------
+    session_name : str
+        Name of the session to rename
+    new_session_name : str
+        The new name of the session
+    user : str
+        The user to connect to MBI-XNAT with
+    connection : xnat.Session
+        A XnatPy session to reuse for the command instead of creating a new one
+    loglevel : str
+        The logging level used for the xnat connection
+    """
+    with connect(user, loglevel=loglevel, connection=connection) as mbi_xnat:
+        try:
+            session = mbi_xnat.experiments[session_name]
+        except KeyError:
+            raise XnatUtilsUsageError(
+                "No session named '{}'".format(session_name))
+        mbi_xnat.put(session.uri + '?label={}'.format(new_session_name))
+    print "Successfully renamed '{}' to '{}'".format(session_name,
+                                                     new_session_name)
+
+
 def varget(subject_or_session_id, variable, default='', user=None,
            connection=None, loglevel='ERROR'):
     """
