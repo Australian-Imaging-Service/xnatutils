45418ddbcafa2e286a3d0bc73d782fa976748fed
Tom Close <tom.g.close@gmail.com>
2018-05-09 14:45:00 +1000

    fixed up licence

--- xnatutils/__init__.py
+++ xnatutils/__init__.py
@@ -1,3 +1,18 @@
+# Copyright (c) 2012-2017 Thomas G. Close, Monash Biomedical Imaging,
+# Monash University, Melbourne, Australia
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from .version_ import __version__
 from .base import connect, print_usage_error, print_response_error
 from .get import get, varget
