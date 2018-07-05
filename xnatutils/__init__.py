db8c6fc48b886b27eebfa3c4ed0818bd80fd1840
Thomas G. Close <tom.g.close@gmail.com>
2018-07-05 11:42:22 +1000

    added import of exceptions and error messages

--- xnatutils/__init__.py
+++ xnatutils/__init__.py
@@ -18,7 +18,7 @@ limitations under the License.
 """
 
 from .version_ import __version__
-from .base import connect, print_usage_error, print_response_error
+from .base import connect, print_usage_error, print_response_error, print_info_message
 from .get import get, varget
 from .put import put, varput
 from .ls import ls
